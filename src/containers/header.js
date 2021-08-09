e61c177df0ee54123219779988579a1bf8072c33
Adam Ireland <85191578+haldam9000@users.noreply.github.com>
2021-08-08 21:59:40 -0400

    Update header.js

--- src/containers/header.js
+++ src/containers/header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
 
-export function HeaderContainer({ children }) {
+export function HeaderContainer({ PS5 FIFA }) {
     return (
         <Header>
             <Header.Frame>
-                <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
+                <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="project0" />
                 <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
             </Header.Frame>
-            {children}
+            {PS5 FIFA}
         </Header>
     )
 }
