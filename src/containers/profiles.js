66334415c461113d4e489cfe0bf5f618040ae650
Adam Ireland <85191578+haldam9000@users.noreply.github.com>
2021-08-08 21:58:05 -0400

    Update profiles.js

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -10,7 +10,7 @@ export function SelectProfileContainer({ user, setProfile }) {
                     <Header.Logo 
                         to={ROUTES.HOME} 
                         src="/images/misc/logo.svg"
-                        alt="Netflix"
+                        alt="project0"
                     />
                 </Header.Frame>
             </Header>
