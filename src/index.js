0ad739c3a44279a177f360bb16f31a36c157481f
Adam Ireland <85191578+haldam9000@users.noreply.github.com>
2021-08-08 21:43:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { FirebaseContext } from './context/firebase';
 
 const config = {
   apiKey: 'AIzaSyDAU60M2FDHNsYt9uGMpRezw3FGVIX6Jrs',
-  authDomain: 'netflix-c8ae9.firebaseapp.com',
-  databaseURL: 'https://netflix-c8ae9.firebaseio.com',
-  projectId: 'netflix-c8ae9',
-  storageBucket: 'netflix-c8ae9.appspot.com',
+  authDomain: 'project0-c8ae9.firebaseapp.com',
+  databaseURL: 'https://project0-c8ae9.firebaseio.com',
+  projectId: 'project0-c8ae9',
+  storageBucket: 'project0-c8ae9.appspot.com',
   messagingSenderId: '590575805931',
   appId: '1:590575805931:web:f809092b6cd55306fa25ae',
 };
